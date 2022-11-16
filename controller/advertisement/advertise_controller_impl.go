eefa44c544adfd0e9fcde635410054386ae52db9
Ardan Hilal <ardanhilal@pop-os.localdomain>
2022-11-16 22:03:05 +0700

    advertise controller on going

--- controller/advertisement/advertise_controller_impl.go
+++ controller/advertisement/advertise_controller_impl.go
@@ -1,1 +1,65 @@
 package controller
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/Ardnh/go-ems/exception"
+	"github.com/Ardnh/go-ems/helper"
+	"github.com/Ardnh/go-ems/model/web"
+	service "github.com/Ardnh/go-ems/service/advertisement"
+)
+
+type AdvertiseControllerImpl struct {
+	Service service.AdvertisementService
+}
+
+func NewAdvertsementController(service service.AdvertisementService) AdvertisementController {
+	return &AdvertiseControllerImpl{
+		Service: service,
+	}
+}
+
+func (controller *AdvertiseControllerImpl) Create(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseMultipartForm(32 << 20)
+	if err != nil {
+		webResponse := web.WebResponse{
+			Code:   http.StatusBadRequest,
+			Status: "BAD REQUEST",
+		}
+		helper.WriteToResponseBody(w, webResponse)
+		return
+	}
+
+	userId, _ := strconv.Atoi(r.PostFormValue("user_id"))
+	eventId, _ := strconv.Atoi(r.PostFormValue("event_id"))
+	instagramUrl := r.PostFormValue("instagram_url")
+	twitterUrl := r.PostFormValue("twitter_url")
+	facebookUrl := r.PostFormValue("facebook_url")
+	file, _, errParseImage := r.FormFile("banner")
+	if errParseImage != nil {
+		exception.InternalServerError(w, r, errParseImage)
+	}
+
+	url, errUpload := helper.ImageUpload(file, "CLOUDINARY_ADVERTISE_FOLDER_NAME")
+	if errParseImage != nil {
+		exception.InternalServerError(w, r, errUpload)
+	}
+
+	advertiseCreateRequest := web.AdvertiseCreateRequest{
+		UserId:       userId,
+		EventId:      eventId,
+		InstagramUrl: instagramUrl,
+		TwitterUrl:   twitterUrl,
+		FacebookUrl:  facebookUrl,
+		BannerUrl:    url,
+	}
+
+	advertiseResponse := controller.Service.Create(r.Context(), advertiseCreateRequest)
+	webResponse := web.WebResponse{
+		Code:   http.StatusBadRequest,
+		Status: "BAD REQUEST",
+		Data:   advertiseResponse,
+	}
+	helper.WriteToResponseBody(w, webResponse)
+}
---
+++ helper/cldnr.go
@@ -0,0 +1,27 @@
+package helper
+
+import (
+	"context"
+	"time"
+
+	"github.com/cloudinary/cloudinary-go"
+	"github.com/cloudinary/cloudinary-go/api/uploader"
+)
+
+func ImageUpload(image interface{}, folder string) (string, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	//create cloudinary instance
+	cld, err := cloudinary.NewFromParams(LoadEnvFile("CLOUDINARY_CLOUD_NAME"), LoadEnvFile("CLOUDINARY_API_KEY"), LoadEnvFile("CLOUDINARY_API_SECRECT_KEY"))
+	if err != nil {
+		return "", err
+	}
+
+	uploadParam, err := cld.Upload.Upload(ctx, image, uploader.UploadParams{Folder: folder})
+	if err != nil {
+		return "", err
+	}
+
+	return uploadParam.SecureURL, nil
+}
